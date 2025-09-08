733a9418a07ae189aacf95bbf3cf0be9bbdbe1c9
NeshayBah <53318360+NeshayBah@users.noreply.github.com>
2025-09-09 10:50:56 +1200

    Update site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/neshaybahs-chatroom/',   // ⬅ replace with your repo name
+  base: '/neshaybahs-chatroom/',  
 })
