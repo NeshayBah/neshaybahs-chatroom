1d66c316010c24415f850accd1ead61aafea008b
NeshayBah <53318360+NeshayBah@users.noreply.github.com>
2025-09-08 20:51:38 +1200

    Updated chat app with fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/neshaybahs-chatroom/',   // ⬅ replace with your repo name
 })
